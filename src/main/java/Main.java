d6ef7deb7f264e48971396ffbaf342861e822806
Ajay <ajaykumar.88266@gmail.com>
2023-04-22 00:40:55 +0530

    jar file added

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import com.example.ecommerceapplication.ECommerceApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        ECommerceApplication.main(args);
+    }
+}
